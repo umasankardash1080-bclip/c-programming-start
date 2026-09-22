54972b34f317770a20b9e483e826e333b01e53ff
Uma Sankar dash <umasankardash1080@gmail.com>
2026-09-22 19:54:05 +0530

    Add files via upload

---
+++ print number from 0 to 4.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+//	int i = 0;
+	int num;
+	printf("enter your number :");
+	scanf("%d",&num);
+//	while(i<=num){
+//		printf("%d\n",i);
+//		i ++;
+//	}
+    for(int i =0;i<=num;i++){
+    	printf("%d\n",i);
+	}
+	return 0;
+}
